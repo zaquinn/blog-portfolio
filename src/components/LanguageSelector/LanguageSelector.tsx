adb5027ce457053a5ea3baf1c1454cc1bb37a323
zaquinn <ayzick120@gmail.com>
2024-08-28 15:14:01 -0300

    feat: add navigation menu, add mobile initial version, add react island for language selection

---
+++ src/components/LanguageSelector/LanguageSelector.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./style.scss";
+
+function LanguageSelector() {
+  const [language, setLanguage] = useState("pt/BR");
+
+  const toggle = () =>
+    setLanguage((currentLanguage) =>
+      currentLanguage === "pt/BR" ? "en/US" : "pt/BR"
+    );
+
+  return (
+    <button
+      className="flex-center language-selector px-1 py-1 mt-3 mr-3"
+      onClick={toggle}
+    >
+      {language.toUpperCase()}
+    </button>
+  );
+}
+
+export default LanguageSelector;
